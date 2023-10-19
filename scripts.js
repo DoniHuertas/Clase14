9749eb693c9f657a4ce465457d735e4b372a8290
Daniel Huertas <razihel21@hotmail.com>
2023-10-18 21:29:01 -0300

    Primer commit

---
+++ scripts.js
@@ -0,0 +1,24 @@
+console.log("Hola desde la terminal de Chrome y del Visual");
+
+let nombre = "Daniel";
+nombre = "Facundo";
+
+console.log(nombre);
+let miRequest = {
+  method: "POST",
+  body: {
+    user: "Doni123",
+    pass: "Abc456",
+  },
+  protocol: "ej HTTP1",
+  headers: "Información complementaria",
+  url: "A que dirección realizamos el pedido",
+};
+
+// let miResponse = {
+//   statusCode: ["1xx", "2xx", "3xx", "4xx", "5xx"], // 202 -> Not found
+//   statusMessage: "Usuario creado",
+//   body: "Dependiendo del tipo de respuesta, puede existir un body",
+//   protocol: "ej HTTP1",
+//   headers: "Información complementaria",
+// };
