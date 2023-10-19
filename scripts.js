25934a35097be7073fe78bc11aa97550bd52df23
Daniel Huertas <cesard.huertas@gmail.com>
2023-10-18 21:53:32 -0300

    Cambios en el scripts.js

--- scripts.js
+++ scripts.js
@@ -15,10 +15,10 @@ let miRequest = {
   url: "A que dirección realizamos el pedido",
 };
 
-// let miResponse = {
-//   statusCode: ["1xx", "2xx", "3xx", "4xx", "5xx"], // 202 -> Not found
-//   statusMessage: "Usuario creado",
-//   body: "Dependiendo del tipo de respuesta, puede existir un body",
-//   protocol: "ej HTTP1",
-//   headers: "Información complementaria",
-// };
+let miResponse = {
+  statusCode: ["1xx", "2xx", "3xx", "4xx", "5xx"], // 202 -> Not found
+  statusMessage: "Usuario creado",
+  body: "Dependiendo del tipo de respuesta, puede existir un body",
+  protocol: "ej HTTP1",
+  headers: "Información complementaria",
+};
